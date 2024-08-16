d14cd46567730a2ab02efe098b383baac93f64a0
Artem Denisov <lifeinplus@gmail.com>
2024-08-16 16:19:25 +0200

    Bump version to 4.7.0

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <footer className={`footer flex-wrap bg-${theme} mt-auto py-2`}>
             <div className="container-fluid d-flex align-items-center">
                 <p className="col-5 text-secondary m-0">
-                    v4.6.1 © 2024 Artem Denisov
+                    v4.7.0 © 2024 Artem Denisov
                 </p>
                 <NavLink
                     className="col-2 d-flex justify-content-center"
